42df1e1ea9fbc0406ebe961814f00e4116f2ae30
ShevchenkoOleksii <84289820+ShevchenkoOleksii@users.noreply.github.com>
2022-03-04 23:12:09 +0200

    'app_does_not_work'

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(todoRoutes)
+
+async function start() {
+    try {
+        await mongoose.connect('mongodb+srv://Oleksii:1Cdn0Sw8j0aTJ3lB@cluster0.43our.mongodb.net/todos', {
+
+        })
+
+        // await mongoose.connect('mongodb+srv://Oleksii:1Cdn0Sw8j0aTJ3lB@cluster0.43our.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+        app.listen(PORT, () => {
+            console.log(`Server has been started...`)
+        })
+    } catch (e) {
+
+        console.log(e)
+    }
+}
+
+start()
+
+
+
